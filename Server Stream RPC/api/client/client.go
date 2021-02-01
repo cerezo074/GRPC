9829b69646abd7fe9dd41f20734bbed0989df16b
Eli Pacheco <eli.pacheco@Eli-Pacheco-MacBook-Pro.local>
2021-01-31 22:22:56 -0500

    Implement RPC server stream

---
+++ Server Stream RPC/api/client/client.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"io/ioutil"
+	"os"
+	"service/rpc/imagepb"
+
+	"fmt"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Running Client...")
+
+	clientConnection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect %v", err)
+		return
+	}
+
+	defer clientConnection.Close()
+	imageName := "ferry2.jpg"
+	connection := imagepb.NewImageServiceClient(clientConnection)
+	buffer, err := downloadImage(connection, imageName)
+
+	if buffer != nil {
+		process(buffer, imageName)
+	}
+}
+
+func downloadImage(connection imagepb.ImageServiceClient, imageName string) (*bytes.Buffer, error) {
+	request := imagepb.ImageDownloaderRequest{
+		ImageName: imageName,
+	}
+
+	fileStreamResponse, err := connection.Download(context.Background(), &request)
+	if err != nil {
+		log.Fatalf("Error while calling Download RPC, %v", err)
+		return nil, err
+	}
+
+	buffer := &bytes.Buffer{}
+
+	for {
+		chunkResponse, err := fileStreamResponse.Recv()
+		if err == io.EOF {
+			log.Println("Closing connection with RPC, stream has sent all data")
+			break
+		} else if err != nil {
+			log.Printf("Error while receive chunk data, %v", err)
+			return nil, err
+		}
+
+		log.Printf("Receive new chunk data, secuence %d from total %d", chunkResponse.Result.CurrentSecuence, chunkResponse.Result.LastSecuence)
+
+		if _, err = buffer.Write(chunkResponse.Result.Content); err != nil {
+			log.Printf("Error writing chunk data, %v", err)
+			return nil, err
+		}
+	}
+
+	return buffer, nil
+}
+
+func process(buffer *bytes.Buffer, imageName string) {
+	imageFile := "./images/" + imageName
+	os.Remove(imageFile)
+
+	if err := ioutil.WriteFile(imageFile, buffer.Bytes(), 777); err != nil {
+		log.Printf("Error writing file, %v", err)
+		return
+	}
+
+	log.Println("File " + imageName + " created successfully inside images folder")
+}
---
+++ Server Stream RPC/api/server/server.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"net"
+	"os"
+	"service/rpc/imagepb"
+
+	"google.golang.org/grpc"
+)
+
+type ImageHandler struct {
+	imagepb.UnsafeImageServiceServer
+}
+
+func (*ImageHandler) findImage(filename string, bufferSize int) (*os.File, int64, error) {
+	file, err := os.Open("../../assets/" + filename)
+
+	if err != nil {
+		log.Fatalf("Error opening image file: %v", err)
+		return nil, 0, err
+	}
+
+	stats, err := file.Stat()
+
+	if err != nil {
+		log.Fatalf("Error opening image file: %v", err)
+		return nil, 0, err
+	}
+
+	fileSize := stats.Size()
+
+	return file, fileSize, nil
+}
+
+func (handler *ImageHandler) Download(request *imagepb.ImageDownloaderRequest, responseStream imagepb.ImageService_DownloadServer) error {
+	fmt.Printf("Language detector function was invoked with %v", request)
+	imageName := request.GetImageName()
+	bufferSize := 64 * 1024
+	file, fileSize, err := handler.findImage(imageName, bufferSize)
+	if err != nil {
+		log.Fatalf("Error opening image file, %v", err)
+		return err
+	}
+
+	iterations := int64(math.Ceil(float64(fileSize) / float64(bufferSize)))
+	buffer := make([]byte, bufferSize)
+
+	for i := int64(1); i <= iterations; i++ {
+		bytesRead, err := file.Read(buffer)
+		if err == io.EOF {
+			log.Println("Finish sending file:" + imageName)
+			return nil
+		} else if err != nil {
+			log.Fatalf("Error reading image file, %v", err)
+			return err
+		}
+
+		response := &imagepb.ImageDownloaderResponse{
+			Result: &imagepb.ImageChunk{
+				Content:         buffer[:bytesRead],
+				CurrentSecuence: int32(i),
+				LastSecuence:    int32(iterations),
+				SecuenceSize:    int64(bufferSize),
+			},
+		}
+
+		if err := responseStream.Send(response); err != nil {
+			log.Fatalf("Error sending response, %v", err)
+			return err
+		}
+
+		log.Printf("Sent chunk in secuence %d from total %d secuences", i, iterations)
+	}
+
+	return nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen, %v", err)
+		return
+	}
+
+	grpcServer := grpc.NewServer()
+	imagepb.RegisterImageServiceServer(grpcServer, &ImageHandler{})
+	fmt.Println("Server Listening...")
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve, %v", err)
+	}
+}
